3dedc619f475d0a7a5c9d3f0d16aebcce2389704
Fish <okfish@yandex.ru>
2015-09-11 17:54:13 +0300

    django's admin get started without dashboard

--- oscar_ficta/admin.py
+++ oscar_ficta/admin.py
@@ -1,40 +1,35 @@
 from django.contrib import admin
-#from treebeard.admin import TreeAdmin
-#from treebeard.forms import movenodeform_factory
+from treebeard.admin import TreeAdmin
+from treebeard.forms import movenodeform_factory
 
 from oscar.core.loading import get_model
 
 Person = get_model('oscar_ficta', 'Person')
 PersonGroup = get_model('oscar_ficta', 'PersonGroup')
 LegalAddress = get_model('oscar_ficta', 'LegalAddress')
+Bank = get_model('oscar_ficta', 'Bank')
+BankAccount = get_model('oscar_ficta', 'BankAccount')
+LegalAddress = get_model('oscar_ficta', 'LegalAddress')
 
+class LegalAddressInline(admin.TabularInline):
+    model = LegalAddress
+    extra = 1
 
-#class AttributeInline(admin.TabularInline):
-#    model = ProductAttributeValue
-
-
-#class ProductRecommendationInline(admin.TabularInline):
-#    model = ProductRecommendation
-#    fk_name = 'primary'
-
-
-#class CategoryInline(admin.TabularInline):
-#    model = ProductCategory
-#    extra = 1
-
-
+class BankAccountInline(admin.TabularInline):
+    model = BankAccount
+    extra = 1
 
 class LegalAddressAdmin(admin.ModelAdmin):
     list_display = ('postcode', 'line4', 'line1', 'line2', 'line3')
-    #inlines = [ProductAttributeInline]
 
+class PersonGroupAdmin(TreeAdmin):
+    form = movenodeform_factory(PersonGroup)
 
 class PersonAdmin(admin.ModelAdmin):
     date_hierarchy = 'date_created'
     list_display = ('vatin', 'name',)
     list_filter = ['group', 'is_active', 'status']
-    #inlines = [AttributeInline, CategoryInline, ProductRecommendationInline]
-    prepopulated_fields = {"slug": ("name",)}
+    inlines = [LegalAddressInline, BankAccountInline]
     search_fields = ['name', 'vatin']
 
 #     def get_queryset(self, request):
@@ -46,6 +41,15 @@ class PersonAdmin(admin.ModelAdmin):
 #                 'attribute_values',
 #                 'attribute_values__attribute'))
 
+class BankAdmin(admin.ModelAdmin):
+    list_display = ('bic', 'name',)
+    #list_filter = ['group', 'is_active', 'status']
+    #inlines = [AttributeInline, CategoryInline, ProductRecommendationInline]
+    #prepopulated_fields = {"slug": ("name",)}
+    search_fields = ['name', 'bic']
+
 
 admin.site.register(Person, PersonAdmin)
+admin.site.register(PersonGroup, PersonGroupAdmin)
+admin.site.register(Bank, BankAdmin)
 admin.site.register(LegalAddress, LegalAddressAdmin)
